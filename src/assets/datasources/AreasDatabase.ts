0e04ad3a023395b9969c24cc6e222cf620145c1f
David Benitez <dbr.bcn@gmail.com>
2022-02-08 20:47:04 +0100

    adds areas database

---
+++ src/assets/datasources/AreasDatabase.ts
@@ -0,0 +1,14 @@
+const areasDatabase: Map<String, object> = new Map<string, object>([
+    ["inmigrants", {
+        "title": "ÀREA PROJECTES D'IMMIGRANTS",
+        "description": "Arran del desconeixement del col·lectiu Immigrant dels recursos socials, de la salut, laborals i legals existents als municipis i la major exposició d'aquests col·lectius a situacions de vulnerabiliat socials, com ara: l'aïllament social, manca de suport familiar, condicions laborals i d'habitatge precàries, percepcions diferents sobre la salut, dificultats d'establir una comunicació fluïda sobre temes com la sexualitat i riscos associats, l'ús de mètodes anticonptius i d'hàbits d'alimentació no saludables, més l'agreujament per la situació actual d'atur i pobresa fa que la població immigrant pugui estar en situació de desigualtat social i de salut i és per això que des de Ceps Salut es desenvolupen diferents projectes a l'àmbit de la immigració. Ceps Salut disposa d'una cartera de Serveis formada per quatre eixos de treball: Programes d'intervenció en salut, Educació per a la salut i disseny de materials pedagògics, Formació i assessorament a professionals i Estudis relacionats amb la salut.",
+        "image": "image/path/area1.png"
+    }],
+    ["collectius-vulnerables", {
+        "title": "ÀREA D'ALTRES PROJECTES DE COL·LECTIUS VULNERABLES",
+        "description": "L'objectiu de la nostra entitat és promoure la salut i contribuir-ne a la prevencio afavorint hàbits i entorns saludables per tal de millorar la qualitat de vida de la població en general i dels col·lectius vulnerables en particular, i així reduir les desigualtats en salut. Així mateix, dins de la nostra entitat és un objectiu el sensibilitzar vers la igualtat de gènere, així com impulsar, desenvolupar i implementar polítiques de gènere dins de l'entitat i de totes les activitats, accions i programes que se'n deriven.",
+        "image": "image/path/area2.png"
+    }]
+]);
+
+export default areasDatabase;
