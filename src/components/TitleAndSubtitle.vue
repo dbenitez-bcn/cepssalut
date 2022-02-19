711c056fb9391a329b05846434b782410a495c49
David Benitez <dbr.bcn@gmail.com>
2022-02-19 16:45:21 +0100

    creates title and subtitle component

---
+++ src/components/TitleAndSubtitle.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-container class="d-flex flex-column justify-center">
+    <v-row>
+      <v-col>
+        <div class="title text-center" v-text="title" center></div>
+      </v-col>
+    </v-row>
+    <v-row justify="center" >
+      <v-col sm="6">
+        <div class="body-1 text-justify" v-text="subtitle"></div>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  props: {
+    title: { type: String, required: true },
+    subtitle: { type: String, required: true },
+  },
+});
+</script>
+
+<style scoped>
+</style>
