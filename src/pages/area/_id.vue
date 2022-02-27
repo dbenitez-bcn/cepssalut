21b6b528f420c86085351ec1c42f13825d77a7da
David Benitez <dbr.bcn@gmail.com>
2022-02-27 12:47:36 +0100

    adds area page

---
+++ src/pages/area/_id.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-container class="pt-0">
+    <v-img
+      :src="require(`~/assets/areas/${area.image}`)"
+      max-height="300"
+    ></v-img>
+    <title-and-subtitle
+      :title="area.title"
+      :subtitle="area.description"
+    ></title-and-subtitle>
+    <v-divider></v-divider>
+    <email-box></email-box>
+  </v-container>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Area from "~/domain/Area";
+
+export default Vue.extend({
+  head() {
+    return {
+      title: (this as any).area.id,
+    };
+  },
+
+  data() {
+    return {
+      area: this.$areas.getById(this.$route.params.id),
+    };
+  },
+});
+</script>
+
+<style scoped>
+</style>
