753915ca735313fb24016d0514c3d0b8a46dc5c2
David Benitez <dbr.bcn@gmail.com>
2022-02-27 12:55:40 +0100

    adds di-td project

--- src/assets/datasources/AreasDatabase.ts
+++ src/assets/datasources/AreasDatabase.ts
@@ -8,6 +8,11 @@ const areasDatabase: Map<string, object> = new Map<string, object>([
         "title": "PROJECTES DE COL·LECTIUS VULNERABLES",
         "description": "L'objectiu de la nostra entitat és promoure la salut i contribuir-ne a la prevencio afavorint hàbits i entorns saludables per tal de millorar la qualitat de vida de la població en general i dels col·lectius vulnerables en particular, i així reduir les desigualtats en salut. Així mateix, dins de la nostra entitat és un objectiu el sensibilitzar vers la igualtat de gènere, així com impulsar, desenvolupar i implementar polítiques de gènere dins de l'entitat i de totes les activitats, accions i programes que se'n deriven.",
         "image": "area-collectiu.jpg"
+    }],
+    ["di-td", {
+        "title": "PROJECTES ADREÇATS A PERSONES AMB DISCAPACITAT INTEL·LECTUAL I/O TRASTORNS DEL DESENVOLUPAMENT",
+        "description": "A Ceps Salut, des de l’any 2000 realitzem projectes de Prevenció i Promoció de la Salut pel col·lectiu de les persones amb Discapacitat Intel·lectual (DID) i/o Trastorn Generalitzat del Desenvolupament (TGD). Tots els programes estan adaptats a la realitat de les persones i a les necessitats dels professionals i famílies. Les intervencions estan basades en una metodologia adaptada a les dificultats cognitives, comunicatives i funcionals, per afavorir la ràpida i segura adquisició dels continguts en el treball amb els DID i TGD, facilitant la tasca dels professionals, tant de tècnics com d’atenció directa, i de les famílies, en la millora de la situació de salut mental de les persones que atenen. La base és l’avaluació de les capacitats per concretar les possibilitats de les persones i donar suport en les dificultats, amb l’objectiu d’aconseguir millores en el benestar de la persona i el seu entorn.",
+        "image": "di-td.jpg"
     }]
 ]);
 
