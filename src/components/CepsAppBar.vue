40c353b190a7157a1a4214afba71091b369bbbac
David Benitez <dbr.bcn@gmail.com>
2022-02-27 12:47:12 +0100

    adds routing behaviour

--- src/components/CepsAppBar.vue
+++ src/components/CepsAppBar.vue
@@ -1,22 +1,37 @@
 <template>
-<v-app-bar
-      app
-      color="primary"
-      class="d-flex justify-center justify-sm-start"
-      elevation="0"
-    >
-      <v-toolbar-title >CepsSalut</v-toolbar-title>
-    </v-app-bar>
+  <v-app-bar
+    app
+    color="primary"
+    class="d-flex justify-center justify-sm-start"
+    elevation="0"
+  >
+    <div
+      class="title text-center clickable"
+      v-text="title"
+      @click="toHome"
+    ></div>
+  </v-app-bar>
 </template>
 
 <script lang="ts">
-    import Vue from 'vue'
+import Vue from "vue";
 
-    export default Vue.extend({
-        
-    })
+export default Vue.extend({
+  data() {
+    return {
+      title: "Ceps Salut",
+    };
+  },
+  methods: {
+    toHome() {
+      this.$router.push("/");
+    },
+  },
+});
 </script>
 
 <style scoped>
-
+.clickable:hover {
+  cursor: pointer;
+}
 </style>
