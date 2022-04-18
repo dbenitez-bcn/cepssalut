5d339e53a0f2a8c9b10b847fc6676ec655c4bdea
David Benitez <dbr.bcn@gmail.com>
2022-04-18 17:21:58 +0200

    adds area creation and deletion

--- src/components/AdminView.vue
+++ src/components/AdminView.vue
@@ -1,12 +1,43 @@
 <template>
-  <div>Admin view</div>
+  <div>
+    <v-container>
+      <v-col>
+        <v-row class="add-area" align="center" @click="toCreateArea">
+          <v-icon color="gray" class="pa-2"> mdi-plus </v-icon>
+          Afegir area
+        </v-row>
+      </v-col>
+    <area-list-item v-for="area in areas" :key="area.id" :area="area"></area-list-item>
+    </v-container>
+  </div>
 </template>
 
-<script lang="ts">
+<script>
 import Vue from "vue";
+import AreaListItem from "./AreaListItem.vue";
 
-export default Vue.extend({});
+export default Vue.extend({
+  components: { AreaListItem },
+  data() {
+    return {
+      areas: [],
+    };
+  },
+  async fetch() {
+    this.areas = await this.$areas.getAll();
+  },
+  methods: {
+    toCreateArea() {
+      this.$router.push("/admin/area/create");
+    },
+  },
+});
 </script>
 
 <style scoped>
+.add-area {
+  border: dashed gray;
+  border-radius: 6px;
+  cursor: pointer;
+}
 </style>
